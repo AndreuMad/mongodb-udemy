23584f6c40055efbd80a45c69801d07a4c4df4a6
andriy.labyk <andriy.labyk@thinkmobiles.com>
2017-10-04 18:20:26 +0300

    1

--- src/user.js
+++ src/user.js
@@ -2,5 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    nema: String
+    name: String
 });
+
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
--- test/test_helper.js
+++ test/test_helper.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 
 // "C:\Program Files\MongoDB\Server\3.4\bin\mongod"
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', (error) => {
-        console.warn('Error', error);
+mongoose.Promise = global.Promise;
+
+before((done) => {
+
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => done())
+        .on('error', (error) => {
+            console.warn('Error', error);
+        });
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done();
+    });
 });
