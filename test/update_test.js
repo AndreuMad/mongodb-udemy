8b149c8f41af1119036e66a73f711bb69e08322c
Andrii Kit <andrii.labyk@gmail.com>
2017-10-06 00:42:49 +0300

    1

--- test/update_test.js
+++ test/update_test.js
@@ -6,23 +6,61 @@ describe('Updating records', () => {
 
     beforeEach((done) => {
         joe = new User({
-            name: 'Joe'
+            name: 'Joe',
+            postCount: 0
         });
         joe.save()
             .then(() => done());
     });
 
-    it('Instance type, using set and save', (done) => {
-        joe.set('name', 'Alex');
-        joe.save()
+    function assertName(operation, done) {
+        operation
             .then(() => {
                 return User.find({});
             })
             .then((users) => {
-                console.log(users);
                 assert(users.length === 1);
                 assert(users[0].name === 'Alex');
                 done();
             });
+    }
+
+    it('Instance type, using set and save', (done) => {
+        joe.set({ 'name': 'Alex' });
+        assertName(joe.save(), done);
+    });
+
+    it('A model instance can update', (done) => {
+        assertName(joe.update({ name: 'Alex' }), done);
+    });
+
+    it('A model class can update', (done) => {
+        assertName(
+            User.update({ name: 'Joe' }, { name: 'Alex' }),
+            done
+        );
+    });
+
+    it('A model class can update one record', (done) => {
+        assertName(
+            User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }),
+            done
+        );
+    });
+
+    it('A model class can find by ID and update', (done) => {
+        assertName(
+            User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
+            done
+        );
+    });
+
+    it('A user\'s post count incremented by 1', (done) => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            })
     });
 });
